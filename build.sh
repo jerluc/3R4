b1ecdc70104d4150a2c40c503a0867b265323c6d
jerluc <jeremyalucas@gmail.com>
2016-08-12 17:38:51 -0700

    [WIP] Removing "dangling" Docker images

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker build --force-rm -t era .
+
+# TODO: Is this really okay in all cases?
+# It's supposed to remove all "dangling" images
+docker rmi $(docker images -f "dangling=true" -q)
